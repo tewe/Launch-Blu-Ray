188954dfac7bb075ef38bbbf86591e3f6b85eb4b
tewe <tewe@github>
2014-02-05 16:58:09 +0100

    initial commit

---
+++ mpv.sh
@@ -0,0 +1,1 @@
+/Applications/mpv.app/Contents/MacOS/mpv bd://1 bd://2 bd://3 bd://4 --bluray-device="`dirname "$1"`"
---
+++ platypus.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/platypus -o None -Q /Applications/mpv.app/Contents/Resources/document.icns -p /bin/sh -V 1.2 -I com.apple.automator.mpv -D -B -R -x mpv.sh ~/Applications/mpv.app
