dff2878eadbd1b2a84f2baaca838c65d21026286
tewe <tewe@github>
2014-04-02 15:59:16 +0200

    replace osascript with pure python heuristics

--- mpv.py
+++ mpv.py
@@ -1,15 +1,20 @@
 import os
 
-from moreplistlib import getkind
-
 MPV = '/Applications/mpv.app/Contents/MacOS/mpv'
 
+def isbd(path):
+    index = os.path.join(path, 'index.bdmv')     # TODO does uppercase occur?
+    return os.path.isfile(index)
+
+def isdvd(path):
+    _, ext = os.path.splitext(path)
+    return ext.lower() == '.iso'
+
 def execmpv(path):
-    kind = getkind(path)
-    if kind == 'AVCHD Collection':
+    if isbd(path):
         path = os.path.dirname(path)
         os.execl(MPV, 'mpv','bd://1', 'bd://2', 'bd://3', 'bd://4', '--bluray-device=' + path)
-    elif kind == 'ISO Disk Image':
+    elif isdvd(path):
         os.execl(MPV, 'mpv', 'dvd://', '--dvd-device=' + path)
     else:
         os.execl(MPV, 'mpv', path)
