615f47aaff53034d68c0a4e115f1e4214dffa581
tewe <tewe@github>
2014-04-02 16:02:07 +0200

    switch from bash to python version
    tested with dvd.iso, mounted BD, extracted BD, file.mp4
    using names with whitespace and unicode characters

--- platypus.sh
+++ platypus.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/platypus -o None -Q /Applications/mpv.app/Contents/Resources/document.icns -p /bin/sh -V 1.3 -I com.apple.automator.mpv -D -B -R -y -x mpv.sh ~/Applications/mpv.app
+/usr/local/bin/platypus -o None -Q /Applications/mpv.app/Contents/Resources/document.icns -p /usr/bin/python -V 1.4 -I com.apple.automator.mpv -D -B -R -y -x mpv.py ~/Applications/mpv.app
 python patch.py ~/Applications/mpv.app/Contents/Info.plist
